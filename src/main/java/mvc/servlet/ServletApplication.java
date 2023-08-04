763b7c4437f5d30e6d670bddc05aa4ae0170ecc3
wch-os <wch1224.s@gmail.com>
2023-08-05 04:43:17 +0900

    base

---
+++ src/main/java/mvc/servlet/ServletApplication.java
@@ -0,0 +1,17 @@
+package mvc.servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan //자동으로 서블릿을 찾아서 등록해줌
+@SpringBootApplication
+public class ServletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServletApplication.class, args);
+		System.out.println();
+	}
+
+}
+
