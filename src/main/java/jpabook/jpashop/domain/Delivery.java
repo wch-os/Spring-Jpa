42e1eced50417ad7deedae0b2b6588e93876c347
wch-os <wch1224.s@gmail.com>
2023-08-16 15:22:50 +0900

    섹션2. 엔티티 클래스 개발

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Delivery {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY) //거울
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    /**
+     * 중요!. EnumType.ORDINAL가 default 값이다.
+     * 중간에 enum이 추가되면 순서가 밀리게 된다.
+     */
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status; //READY, COMP
+}
---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,27 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.amqp.RabbitConnectionDetails;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String username;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member") //거울
+    private List<Order> orders = new ArrayList<>();
+}
